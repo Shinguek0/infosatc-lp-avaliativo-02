8bd9e26f1bde051d17002a30cd51baa87c75f43f
Vinicius <Shingueko_@hotmail.com>
2020-09-09 10:39:09 -0300

    Feat: adiciona exercício 07

---
+++ exercicio07.py
@@ -0,0 +1,6 @@
+ListaPC = ["gabinete cooler master", "GeForce RTX 2080Ti", "INTEL CORE I9 9900", "Z390M DDR4", "16GB DDR4 GAMING"]
+
+print("Lista com todos os itens:", ListaPC)
+ListaPC.remove("gabinete cooler master")
+ListaPC.remove("GeForce RTX 2080Ti")
+print("Lista com os itens removidos:", ListaPC)
