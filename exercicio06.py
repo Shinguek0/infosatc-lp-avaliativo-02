1024d5ffbe4c65c00c36320393ddcf20aa81a010
Vinicius <Shingueko_@hotmail.com>
2020-09-09 10:38:55 -0300

    Feat: adiciona exercício 06

---
+++ exercicio06.py
@@ -0,0 +1,8 @@
+ListaFilmes = ["baby driver", "star wars"]
+ListaJogos = ["Adastra", "password"]
+ListaLivros = ["O principe", "Sim"]
+ListaEsporte = ["futebol", "Tenis"]
+
+Lista = ListaFilmes + ListaJogos + ListaLivros + ListaEsporte
+
+print("Item da lista livro:", ListaLivros[0])
